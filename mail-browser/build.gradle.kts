0256a0b6774a900e1b2e92011c77514ea45f1851
Vahelnir <vahelnir@gmail.com>
2023-04-12 10:16:57 +0200

    feat: add angus-mail with jakarta-mail

--- mail-browser/build.gradle.kts
+++ mail-browser/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "fr.leswebdevs"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    implementation("jakarta.mail:jakarta.mail-api:2.1.1")
+    implementation("org.eclipse.angus:angus-mail:2.0.1")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
