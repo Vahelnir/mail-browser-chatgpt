e5c8790e23a5c89f8dbb61a7294c7c471bf32dfa
cosiewicz <christophe.osiewicz@gmail.com>
2023-05-09 20:58:01 +0200

    add Serializable implementation

--- mail-browser/src/main/java/fr/leswebdevs/MailConnectionCredentials.java
+++ mail-browser/src/main/java/fr/leswebdevs/MailConnectionCredentials.java
@@ -4,10 +4,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Builder
-public class MailConnectionCredentials {
+public class MailConnectionCredentials implements Serializable {
     private String imapHost;
     private int imapPort;
     private String smtpHost;
