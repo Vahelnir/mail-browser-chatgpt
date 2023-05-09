4357fa973e146dd2a84b3e0daf1186736a1b11cf
cosiewicz <christophe.osiewicz@gmail.com>
2023-05-09 20:57:37 +0200

    improve code, load config file if exist

--- mail-browser/src/main/java/fr/leswebdevs/MainApp.java
+++ mail-browser/src/main/java/fr/leswebdevs/MainApp.java
@@ -24,6 +24,9 @@ public class MainApp extends Application {
         return instance;
     }
 
+    public static final int SCREEN_WIDTH = 640;
+    public static final int SCREEN_HEIGHT = 480;
+
     @Override
     public void start(Stage stage) throws IOException {
         instance = this;
@@ -32,24 +35,30 @@ public class MainApp extends Application {
 //        } catch (MessagingException e) {
 //            throw new RuntimeException(e);
 //        }
-        URL location = getClass().getResource("/fxml/main.fxml");
+        URL location = null;
+        if (ConfigFile.isExist()) {
+            location = getClass().getResource("/fxml/main.fxml");
+        } else {
+            location = getClass().getResource("/fxml/configConnection.fxml");
+        }
+
         FXMLLoader loader = new FXMLLoader(location);
-        Scene scene = new Scene(loader.load(), 640, 480);
+        Scene scene = new Scene(loader.load(), SCREEN_WIDTH, SCREEN_HEIGHT);
         stage.setScene(scene);
         stage.show();
     }
 
     private CompletableFuture<MailManager> initMailManager() {
-        MailConnectionCredentials credentials = MailConnectionCredentials.builder()
-                .imapHost(System.getenv("IMAP_HOST"))
-                .imapPort(Integer.parseInt(System.getenv("IMAP_PORT")))
-                .smtpHost(System.getenv("SMTP_HOST"))
-                .smtpPort(Integer.parseInt(System.getenv("SMTP_PORT")))
-                .email(System.getenv("EMAIL"))
-                .password(System.getenv("PASSWORD"))
-                .isSSL(System.getenv("IS_SSL").equals("true"))
-                .build();
-        MailManager mailManager = new MailManager(credentials);
+//        MailConnectionCredentials credentials = MailConnectionCredentials.builder()
+//                .imapHost(System.getenv("IMAP_HOST"))
+//                .imapPort(Integer.parseInt(System.getenv("IMAP_PORT")))
+//                .smtpHost(System.getenv("SMTP_HOST"))
+//                .smtpPort(Integer.parseInt(System.getenv("SMTP_PORT")))
+//                .email(System.getenv("EMAIL"))
+//                .password(System.getenv("PASSWORD"))
+//                .isSSL(System.getenv("IS_SSL").equals("true"))
+//                .build();
+        MailManager mailManager = new MailManager(ConfigFile.readFile());
         try {
             return mailManager.connect();
         } catch (MessagingException e) {
