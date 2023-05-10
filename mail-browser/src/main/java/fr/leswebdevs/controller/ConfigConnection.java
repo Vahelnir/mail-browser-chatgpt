af82a16a9ce544bb97cbf657b6e038be379b522a
cosiewicz <christophe.osiewicz@gmail.com>
2023-05-10 12:04:11 +0200

    improve code

--- mail-browser/src/main/java/fr/leswebdevs/controller/ConfigConnection.java
+++ mail-browser/src/main/java/fr/leswebdevs/controller/ConfigConnection.java
@@ -4,6 +4,8 @@ import fr.leswebdevs.ConfigFile;
 import fr.leswebdevs.MailConnectionCredentials;
 import fr.leswebdevs.MailManager;
 import fr.leswebdevs.MainApp;
+import jakarta.mail.MessagingException;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -13,6 +15,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextFormatter;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -47,18 +50,25 @@ public class ConfigConnection implements Initializable {
 
     private static final String MESSAGE_FILE_ERROR = "Erreur lors de l'enregistrement de la configuration";
 
+    private static final String COLOR_RED = "#FF0000";
+    private static final String COLOR_Black = "#000000";
     private MailConnectionCredentials credentials;
 
-    public void onClickValidate() {
-        try {
-            setMessageError("", false);
-            tryToConnect();
-            writeFile();
-
-            showNextScene();
-        } catch (Exception e) {
-            setMessageError(e.getMessage(), true);
-        }
+    public void onClickValidate() throws MessagingException {
+        setMessageError("Connexion...", true, COLOR_Black);
+        MailManager mail = tryToConnect();
+        mail.connect().whenComplete((message, ex) -> {
+            if (ex != null) {
+                setMessageError(MESSAGE_CONNECTION_ERROR, true, COLOR_RED);
+            } else {
+                try {
+                    writeFile();
+                    showNextScene();
+                } catch (Exception e) {
+                    setMessageError(MESSAGE_FILE_ERROR, true, COLOR_RED);
+                }
+            }
+        });
     }
 
     private void writeFile() throws Exception {
@@ -67,37 +77,42 @@ public class ConfigConnection implements Initializable {
         }
     }
 
-    private void tryToConnect() throws Exception {
-        try {
-            this.credentials = MailConnectionCredentials.builder()
-                    .email(this.configEmail.getText())
-                    .password(this.configPassword.getText())
-                    .smtpHost(this.configAddrSMTP.getText())
-                    .smtpPort(Integer.parseInt(this.configPortSMTP.getText()))
-                    .imapHost(this.configAddrIMAP.getText())
-                    .imapPort(Integer.parseInt(this.configPortIMAP.getText()))
-                    .isSSL(this.configSSL.isSelected())
-                    .build();
-            MailManager mailManager = new MailManager();
-            mailManager.setCredentials(credentials);
-            mailManager.connect().get();
-        } catch (Exception e) {
-            throw new Exception(MESSAGE_CONNECTION_ERROR);
-        }
-
+    private MailManager tryToConnect() {
+        this.credentials = MailConnectionCredentials.builder()
+                .email(this.configEmail.getText())
+                .password(this.configPassword.getText())
+                .smtpHost(this.configAddrSMTP.getText())
+                .smtpPort(Integer.parseInt(this.configPortSMTP.getText()))
+                .imapHost(this.configAddrIMAP.getText())
+                .imapPort(Integer.parseInt(this.configPortIMAP.getText()))
+                .isSSL(this.configSSL.isSelected())
+                .build();
+        MailManager mailManager = new MailManager();
+        mailManager.setCredentials(credentials);
+        return mailManager;
     }
 
-    private void showNextScene() throws IOException {
-        Stage stage = (Stage) configAnchorPane.getScene().getWindow();
-        URL location = getClass().getResource("/fxml/main.fxml");
-        Scene scene = new Scene(new FXMLLoader(location).load(), MainApp.SCREEN_WIDTH, MainApp.SCREEN_HEIGHT);
-        stage.setScene(scene);
-        stage.show();
+    private void showNextScene() {
+        Platform.runLater(() -> {
+            Stage stage = (Stage) configAnchorPane.getScene().getWindow();
+            URL location = getClass().getResource("/fxml/main.fxml");
+            Scene scene = null;
+            try {
+                scene = new Scene(new FXMLLoader(location).load(), MainApp.SCREEN_WIDTH, MainApp.SCREEN_HEIGHT);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            stage.setScene(scene);
+            stage.show();
+        });
     }
 
-    private void setMessageError(String message, boolean isVisible) {
-        configMessageError.setText(message);
-        configMessageError.setVisible(isVisible);
+    private void setMessageError(String message, boolean isVisible, String hexColor) {
+        Platform.runLater(() -> {
+            configMessageError.setText(message);
+            configMessageError.setTextFill(Color.web(hexColor));
+            configMessageError.setVisible(isVisible);
+        });
     }
 
     @Override
@@ -114,6 +129,4 @@ public class ConfigConnection implements Initializable {
         return new TextFormatter<>(change ->
                 (change.getControlNewText().matches(regex)) ? change : null);
     }
-
-
 }
