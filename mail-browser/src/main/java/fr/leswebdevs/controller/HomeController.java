08642ad7a3569a3ef0ad243fa2816b2f315b2015
cosiewicz <christophe.osiewicz@gmail.com>
2023-05-11 19:04:06 +0200

    Add pagination feature

--- mail-browser/src/main/java/fr/leswebdevs/controller/HomeController.java
+++ mail-browser/src/main/java/fr/leswebdevs/controller/HomeController.java
@@ -7,10 +7,14 @@ import fr.leswebdevs.dto.MailTableItem;
 import jakarta.mail.*;
 import jakarta.mail.event.ConnectionEvent;
 import jakarta.mail.event.ConnectionListener;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Pagination;
 import javafx.scene.control.TableView;
 
 import java.net.URL;
@@ -29,33 +33,58 @@ public class HomeController implements Initializable {
     private final ObservableList<MailTableItem> mails = FXCollections.observableArrayList();
     @FXML
     public TableView<MailTableItem> mailTableView;
+    @FXML
+    public Pagination mailPagination;
     private MailManager mailManager;
     private Folder currentFolder;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         MainApp mainApp = MainApp.getInstance();
         mailTableView.setItems(mails);
         mainApp.getMailManager()
-            .thenCompose((mailManager) -> {
-                this.mailManager = mailManager;
-                return openInbox();
-            })
-            .thenApply(this::setFolder)
-            .thenApply(this::getMessageCount)
-            .thenAccept((messageCount) -> {
-                loadMessages(messageCount - MAIL_PER_PAGE + 1, messageCount);
-            })
-            .whenComplete((s, error) -> {
-                // TODO: handle error
-                System.out.println(error);
-            });
+                .thenCompose((mailManager) -> {
+                    this.mailManager = mailManager;
+                    return openInbox();
+                })
+                .thenApply(this::setFolder)
+                .thenApply(this::getMessageCount)
+                .thenAccept((messageCount) -> {
+                    loadMessages(messageCount - MAIL_PER_PAGE + 1, messageCount);
+                    initPagination(messageCount);
+
+                })
+                .whenComplete((s, error) -> {
+                    // TODO: handle error
+                    System.out.println(error);
+                });
+    }
+
+    private void initPagination(int messageCount) {
+        Platform.runLater(() -> {
+            try {
+                mailPagination.setPageCount(currentFolder.getMessages().length / MAIL_PER_PAGE);
+                mailPagination.currentPageIndexProperty().addListener(new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                        System.out.println(newValue.intValue());
+                        int from = messageCount - ((newValue.intValue() + 1) * MAIL_PER_PAGE);
+                        loadMessages(from + 1, from + MAIL_PER_PAGE);
+                    }
+                });
+
+            } catch (MessagingException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
     }
 
     private Folder setFolder(Folder folder) {
         currentFolder = folder;
         return folder;
     }
+
 
     private void loadMessages(int from, int to) {
         try {
@@ -68,14 +97,14 @@ public class HomeController implements Initializable {
             currentFolder.fetch(messages, fp);
 
             List<MailTableItem> mailTableItems = Arrays.stream(messages)
-                .map(message -> {
-                    try {
-                        return MailTableItemAdapter.from(message);
-                    } catch (MessagingException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .toList();
+                    .map(message -> {
+                        try {
+                            return MailTableItemAdapter.from(message);
+                        } catch (MessagingException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .toList();
 
             mails.setAll(mailTableItems);
             System.out.println("updating messages");
@@ -94,39 +123,39 @@ public class HomeController implements Initializable {
 
     private CompletableFuture<Folder> openInbox() {
         return CompletableFuture
-            .supplyAsync(() -> {
-                Store store = mailManager.getReadStore();
-                try {
-                    return store.getFolder("INBOX");
-                } catch (MessagingException e) {
-                    throw new CompletionException(e);
-                }
-            })
-            .thenCompose((folder) -> {
-                CompletableFuture<Folder> folderCompletableFuture = new CompletableFuture<>();
-                folder.addConnectionListener(new ConnectionListener() {
-                    @Override
-                    public void opened(ConnectionEvent e) {
-                        folderCompletableFuture.complete(folder);
+                .supplyAsync(() -> {
+                    Store store = mailManager.getReadStore();
+                    try {
+                        return store.getFolder("INBOX");
+                    } catch (MessagingException e) {
+                        throw new CompletionException(e);
                     }
+                })
+                .thenCompose((folder) -> {
+                    CompletableFuture<Folder> folderCompletableFuture = new CompletableFuture<>();
+                    folder.addConnectionListener(new ConnectionListener() {
+                        @Override
+                        public void opened(ConnectionEvent e) {
+                            folderCompletableFuture.complete(folder);
+                        }
 
-                    @Override
-                    public void disconnected(ConnectionEvent e) {
-                    }
+                        @Override
+                        public void disconnected(ConnectionEvent e) {
+                        }
 
-                    @Override
-                    public void closed(ConnectionEvent e) {
+                        @Override
+                        public void closed(ConnectionEvent e) {
+                        }
+                    });
+
+                    // on n'a pas besoin de modifier quoi que ce soit pour le moment
+                    try {
+                        folder.open(Folder.READ_ONLY);
+                    } catch (MessagingException e) {
+                        throw new CompletionException(e);
                     }
+                    return folderCompletableFuture;
                 });
-
-                // on n'a pas besoin de modifier quoi que ce soit pour le moment
-                try {
-                    folder.open(Folder.READ_ONLY);
-                } catch (MessagingException e) {
-                    throw new CompletionException(e);
-                }
-                return folderCompletableFuture;
-            });
     }
 
 }
